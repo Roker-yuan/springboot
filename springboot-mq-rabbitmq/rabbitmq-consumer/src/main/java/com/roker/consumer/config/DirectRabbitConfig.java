39d1c76f9ee164b453164011c9074d4e100935b9
Roker <lpy962464@163.com>
2022-09-11 21:53:57 +0800

    新增rabbitmq-consumer

---
+++ springboot-mq-rabbitmq/rabbitmq-consumer/src/main/java/com/roker/consumer/config/DirectRabbitConfig.java
@@ -0,0 +1,33 @@
+package com.roker.consumer.config;
+
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+/**
+ * @作者: Roker
+ * @时间: 2022/9/11 21:12
+ * @Copyright: Don`t be the same,be better!
+ */
+public class DirectRabbitConfig {
+    //队列 起名：TestDirectQueue
+    @Bean
+    public Queue TestDirectQueue() {
+        return new Queue("TestDirectQueue",true);
+    }
+
+    //Direct交换机 起名：TestDirectExchange
+    @Bean
+    DirectExchange TestDirectExchange() {
+        return new DirectExchange("TestDirectExchange");
+    }
+
+    //绑定  将队列和交换机绑定, 并设置用于匹配键：TestDirectRouting
+    @Bean
+    Binding bindingDirect() {
+        return BindingBuilder.bind(TestDirectQueue()).to(TestDirectExchange()).with("TestDirectRouting");
+    }
+
+}
---
+++ springboot-mq-rabbitmq/rabbitmq-consumer/src/main/java/com/roker/consumer/recevier/DirectReceiver.java
@@ -0,0 +1,22 @@
+package com.roker.consumer.recevier;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+/**
+ * @作者: Roker
+ * @时间: 2022/9/11 21:12
+ * @Copyright: Don`t be the same,be better!
+ */
+@Component
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+ 
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+ 
+}
