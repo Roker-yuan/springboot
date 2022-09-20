ffdeaab79f6e570c356ed914212ae1ae7d441515
Roker <lpy962464@163.com>
2022-09-20 10:34:53 +0800

    add: springboot-cloud-eureka

---
+++ springboot-cloud-eureka/eureka-server/src/main/java/com/roker/server/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.roker.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //过滤eureka路径
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
