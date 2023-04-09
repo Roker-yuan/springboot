cab304094aa33a314728f86c40133462b1373e65
liupengyuan <lpy962464@163.com>
2023-04-09 10:17:04 +0800

    新增spring boot 整合 Druid

---
+++ springboot-jdbc-druid/src/main/java/com/roker/springbootjdbcdruid/SpringbootJdbcDruidApplication.java
@@ -0,0 +1,13 @@
+package com.roker.springbootjdbcdruid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJdbcDruidApplication.class, args);
+    }
+
+}
---
+++ springboot-jdbc-druid/src/test/java/com/roker/springbootjdbcdruid/SpringbootJdbcDruidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roker.springbootjdbcdruid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcDruidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
